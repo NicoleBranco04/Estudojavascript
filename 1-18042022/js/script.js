2b43634a052e0caef2b1e5adb9720a6fc996f555
NICOLE ALICE GOMES BRANCO <nicole.agbranco@senacsp.edu.br>
2022-04-18 12:03:03 -0300

    estilização do javascript

--- 1-18042022/js/script.js
+++ 1-18042022/js/script.js
@@ -1,28 +1,79 @@
 // janela de alerta criada em javascript//
-alert("Olá. Seja bem vindo");
+
+function bemvindo() {
+  alert("Olá. Seja bem vindo");
+}
+
 // vamos solicitar ao usuário que entre com o seu nome
 // utilizaremos o prompt associado a uma variável
-/*var nome = "";
- nome = prompt("Digite o seu nome");
-alert(nome.toUpperCase()); 
-*/
 
-// Calcular as 5 operações aritmeticas
-var numero1 = prompt("Digite um número");
-var numero2 = prompt("Digite outro número");
+function nome() {
+  var nome = "";
+  nome = prompt("Digite o seu nome");
+  alert(nome.toUpperCase());
+}
 
-var soma = parseInt(numero1) + parseInt(numero2);
-var subtrair = numero1 - numero2;
-var multiplicar = numero1 * numero2;
-var dividir = numero1 / numero2;
-var resto = numero1 % numero2;
+function operacoes() {
+  // Calcular as 5 operações aritmeticas
+  var numero1 = prompt("Digite um número");
+  var numero2 = prompt("Digite outro número");
 
-console.log(
-    "os resultados são: \n\n"+
-    "soma: "+soma+
-    "\nsubtração: "+subtrair+
-    "\nmultiplicação: "+multiplicar+
-    "\ndivisão: "+dividir+
-    "n\resto: "+resto
-)
+  var soma = parseInt(numero1) + parseInt(numero2);
+  var subtrair = numero1 - numero2;
+  var multiplicar = numero1 * numero2;
+  var dividir = numero1 / numero2;
+  var resto = numero1 % numero2;
 
+  document.getElementsByTagName("button")[0].innerHTML =
+    "os resultados são: <br><br>" +
+    "soma: " +
+    soma +
+    "<br>subtração: " +
+    subtrair +
+    "<br>multiplicação: " +
+    multiplicar +
+    "<br>divisão: " +
+    dividir +
+    "<br>resto: " +
+    resto;
+}
+
+function operacoescomDiv() {
+  // Calcular as 5 operações aritmeticas
+  var numero1 = prompt("Digite um número");
+  var numero2 = prompt("Digite outro número");
+
+  var soma = parseInt(numero1) + parseInt(numero2);
+  var subtrair = numero1 - numero2;
+  var multiplicar = numero1 * numero2;
+  var dividir = numero1 / numero2;
+  var resto = numero1 % numero2;
+
+  for (var i = 0; i <= 2; i++) {
+    document.getElementsByTagName("div")[i].innerHTML =
+      "os resultados são: <br><br>" +
+      "soma: " +
+      soma +
+      "<br>subtração: " +
+      subtrair +
+      "<br>multiplicação: " +
+      multiplicar +
+      "<br>divisão: " +
+      dividir +
+      "<br>resto: " +
+      resto;
+    document.getElementsByTagName("div")[i].style.padding = "30px";
+    document.getElementsByTagName("div")[i].style.backgroundColor =
+      "rgb(" + 100 * i + ",200,100)";
+    document.getElementsByTagName("div")[i].style.margin = "30px";
+  }
+}
+
+function tamanhoDiv() {
+  document.getElementsByTagName("div")[0].style.width = "50%";
+  document.getElementsByTagName("div")[0].style.transition = " ease 0.5s";
+}
+
+function tamanhoNormalDiv() {
+  document.getElementsByTagName("div")[0].style.width = "100%";
+}
